fe657aafe43c4bf4ee92e012e059f93f3e863d35
kichul7493 <kichul7493@gmail.com>
2024-07-02 16:51:12 +0900

    feat: Add Button component for reusable button functionality

---
+++ src/components/shared/Button/Button.tsx
@@ -0,0 +1,73 @@
+import MuiButton from "@mui/material/Button";
+
+/**
+ * Props for the Button component.
+ */
+interface ButtonProps {
+  /**
+   * The variant of the button.
+   */
+  variant?: "outlined" | "contained";
+  /**
+   * The color of the button.
+   */
+  color?: "primary";
+  /**
+   * Whether the button is disabled.
+   */
+  disabled?: boolean;
+  /**
+   * The click event handler for the button.
+   */
+  onClick?: () => void;
+  /**
+   * The size of the button.
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * The content of the button.
+   */
+  children: React.ReactNode;
+  /**
+   * Whether the button should take up the full width.
+   */
+  fullWidth?: boolean;
+
+  /**
+   * The type of the button.
+   */
+  type?: "button" | "submit" | "reset";
+}
+
+/**
+ * A customizable button component.
+ *
+ * @param props - The props for the Button component.
+ * @returns The rendered Button component.
+ */
+const Button = ({
+  variant = "contained",
+  color = "primary",
+  disabled = false,
+  onClick,
+  size = "large",
+  children,
+  fullWidth = false,
+  type = "button",
+}: ButtonProps) => {
+  return (
+    <MuiButton
+      fullWidth={fullWidth}
+      variant={variant}
+      color={color}
+      disabled={disabled}
+      onClick={onClick}
+      size={size}
+      type={type}
+    >
+      {children}
+    </MuiButton>
+  );
+};
+
+export default Button;
