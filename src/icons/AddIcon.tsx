461b873454eeb71e453bc8fcbde801893884c8d4
kichul7493 <kichul7493@gmail.com>
2024-07-03 15:46:23 +0900

    feat: Add AddIcon component for displaying an "add" icon

---
+++ src/icons/AddIcon.tsx
@@ -0,0 +1,24 @@
+const AddIcon = () => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_6398_473)">
+        <circle cx="24" cy="24" r="24" fill="#499F92" />
+        <line x1="24" y1="12" x2="24" y2="36" stroke="white" stroke-width="2" />
+        <line x1="12" y1="23" x2="36" y2="23" stroke="white" stroke-width="2" />
+      </g>
+      <defs>
+        <clipPath id="clip0_6398_473">
+          <rect width="48" height="48" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default AddIcon;
