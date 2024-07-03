5347bdcdd1c4257afef0e328dd8a6ec34e3cfd4d
kichul7493 <kichul7493@gmail.com>
2024-07-03 15:48:10 +0900

    feat: Add main page component with summary of diary entries

---
+++ src/pages/main.tsx
@@ -0,0 +1,15 @@
+import Typography from "../components/shared/Typography";
+
+const Main = () => {
+  return (
+    <main className="px-8 text-center pt-[60px]">
+      <Typography variant="h5">
+        성장일기님의
+        <br />
+        일기를 요약해드려요.
+      </Typography>
+    </main>
+  );
+};
+
+export default Main;
