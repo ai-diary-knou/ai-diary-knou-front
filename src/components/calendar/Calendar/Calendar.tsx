8c8132237b9c7fb3d5898656d6650112c625debd
silveriron <kichul7493@gmail.com>
2024-07-04 14:28:28 +0900

    feat: Add Calendar component for displaying a calendar with date pickers

---
+++ src/components/calendar/Calendar/Calendar.tsx
@@ -0,0 +1,57 @@
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
+import Badge from "@mui/material/Badge";
+import { PickersDay, PickersDayProps } from "@mui/x-date-pickers/PickersDay";
+import { Dayjs } from "dayjs";
+
+function ServerDay(
+  props: PickersDayProps<Dayjs> & { highlightedDays?: number[] }
+) {
+  const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+  const isSelected =
+    !props.outsideCurrentMonth &&
+    highlightedDays.indexOf(props.day.date()) >= 0;
+
+  return (
+    <Badge
+      key={props.day.toString()}
+      color="primary"
+      overlap="circular"
+      variant={isSelected ? "dot" : undefined}
+    >
+      <PickersDay
+        {...other}
+        outsideCurrentMonth={outsideCurrentMonth}
+        day={day}
+      />
+    </Badge>
+  );
+}
+
+interface CalendarProps {
+  onSelectDate: (date: string) => void;
+}
+
+const Calendar = ({ onSelectDate }: CalendarProps) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        onChange={(value: Dayjs) => onSelectDate(value.format("YYYY-MM-DD"))}
+        slots={{
+          day: ServerDay,
+        }}
+        slotProps={{
+          day: {
+            highlightedDays: [1, 2, 3],
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          } as any,
+        }}
+      />
+    </LocalizationProvider>
+  );
+};
+
+export default Calendar;
---
+++ src/components/calendar/DayCard/DayCard.tsx
@@ -0,0 +1,53 @@
+import dayjs from "dayjs";
+import "dayjs/locale/ko"; // 한국어 로케일을 가져옵니다
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import weekday from "dayjs/plugin/weekday";
+import Typography from "../../shared/Typography";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(weekday);
+dayjs.locale("ko"); // 한국어 로케일을 설정합니다
+
+interface DayCardProps {
+  date: string;
+}
+
+const dummyData = [
+  {
+    date: "2024-07-01",
+    summary: "오늘은 행복한 날이었어요",
+  },
+  {
+    date: "2024-07-02",
+    summary: "오늘은 힘들었어요",
+  },
+  {
+    date: "2024-07-03",
+    summary: "오늘은 행복한 날이었어요",
+  },
+];
+
+const DayCard = ({ date }: DayCardProps) => {
+  const data = dummyData.find((item) => item.date === date);
+
+  if (date === "" || data === undefined) {
+    return <></>;
+  }
+
+  return (
+    <div className="w-full p-2 bg-main_color rounded-lg">
+      <div className="mb-3">
+        <Typography variant="subtitle1" textAlign="left" color="white">
+          {dayjs(data?.date).format("YYYY년 M월 D일 dddd")}
+        </Typography>
+      </div>
+      <div className="mb-3">
+        <Typography variant="body1" textAlign="left" color="white">
+          {data?.summary}
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default DayCard;
