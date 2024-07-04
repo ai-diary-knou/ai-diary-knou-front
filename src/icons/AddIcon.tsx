2b5df5ab518f65647fa59a6057304d84c60c3d8d
silveriron <kichul7493@gmail.com>
2024-07-04 14:28:57 +0900

    refactor: Fix typo in AddIcon component

--- src/icons/AddIcon.tsx
+++ src/icons/AddIcon.tsx
@@ -7,10 +7,10 @@ const AddIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_6398_473)">
+      <g clipPath="url(#clip0_6398_473)">
         <circle cx="24" cy="24" r="24" fill="#499F92" />
-        <line x1="24" y1="12" x2="24" y2="36" stroke="white" stroke-width="2" />
-        <line x1="12" y1="23" x2="36" y2="23" stroke="white" stroke-width="2" />
+        <line x1="24" y1="12" x2="24" y2="36" stroke="white" strokeWidth="2" />
+        <line x1="12" y1="23" x2="36" y2="23" stroke="white" strokeWidth="2" />
       </g>
       <defs>
         <clipPath id="clip0_6398_473">
