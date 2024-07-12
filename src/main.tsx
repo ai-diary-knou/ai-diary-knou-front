9ecc660a31adfcad9c9a9be8e067701ef49310b6
juwon <juone2@sqnt.co.kr>
2024-07-12 09:16:59 +0900

    add mock

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,28 @@ import router from "./router.tsx";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
 import ThemeProvider from "./providers/ThemeProvider.tsx";
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
 
+  const { worker } = await import("./mocks/browser");
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ReactQueryProvider>
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <ReactQueryProvider>
       <ThemeProvider>
-        <Provider store={store}>
-          <RouterProvider router={router} />
-        </Provider>
+          <Provider store={store}>
+            <RouterProvider router={router} />
+          </Provider>
       </ThemeProvider>
-    </ReactQueryProvider>
-  </React.StrictMode>
-);
+      </ReactQueryProvider>
+    </React.StrictMode>
+  );
+});
