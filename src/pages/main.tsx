5251d035931c7fa20cf3cbdaebac636998f6afa9
silveriron <kichul7493@gmail.com>
2024-07-15 20:18:59 +0900

    feat: Update MainPage component to fetch and display data from API

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,20 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
 import EmotionChart from "../components/main/EmotionChart";
 import KeywordList from "../components/main/Keyword";
 import RecommendList from "../components/main/Recommend";
 import Slider from "../components/main/Slider";
 import Typography from "../components/shared/Typography";
-
-const dummyItems = [
-  "좋은 꿈을 꿔서 즐거운 아침을 맞이했어요.",
-  "오늘은 친구들과 함께 즐거운 시간을 보냈어요.",
-  "오늘은 책을 읽으면서 즐거운 시간을 보냈어요.",
-  "오늘은 새로운 친구를 사귀어서 기뻤어요.",
-  "오늘은 새로운 경험을 했어요.",
-  "오늘은 즐거운 일이 있어 기분이 좋았어요.",
-  "오늘은 새로운 것을 배워서 기뻤어요.",
-];
+import axios from "axios";
+import { DIARY_URL_PREFIX } from "../mocks/diary/handlers";
+import { useEffect } from "react";
+import { USER_URL_PREFIX } from "../mocks/users/handlers";
 
 const MainPage = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["main-reports"],
+    queryFn: async () => {
+      const response = await axios.get(DIARY_URL_PREFIX + "/main-reports");
+
+      return response.data.data;
+    },
+  });
+
+  useEffect(() => {
+    axios
+      .get(USER_URL_PREFIX + "/duplicate", {
+        params: {
+          type: "email",
+          value: "test@gmail.com",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
+
+  if (isLoading || data === undefined) {
+    return <></>;
+  }
+
   return (
     <div className="px-8 py-[60px]">
       <Typography variant="h5">
@@ -23,7 +44,7 @@ const MainPage = () => {
         일기를 요약해드려요.
       </Typography>
       <div className="my-[60px]">
-        <Slider items={dummyItems} />
+        <Slider items={data.recentLiterarySummaries} />
       </div>
       <div>
         <div className="mb-3">
@@ -32,7 +53,7 @@ const MainPage = () => {
           </Typography>
         </div>
         <div className="bg-white px-4 pt-4 rounded-lg shadow-sm mb-[60px]">
-          <EmotionChart />
+          <EmotionChart emotionData={data.recentAverageEmotionScales} />
         </div>
       </div>
       <div className="mb-[60px]">
@@ -41,7 +62,7 @@ const MainPage = () => {
             나는...
           </Typography>
         </div>
-        <KeywordList />
+        <KeywordList keywordList={data.recentRepetitiveKeywords} />
       </div>
       <div className="pb-[60px]">
         <div className="mb-3">
@@ -49,7 +70,7 @@ const MainPage = () => {
             추천 목록
           </Typography>
         </div>
-        <RecommendList />
+        <RecommendList recommendList={data.recentRecommendedActions} />
       </div>
     </div>
   );
