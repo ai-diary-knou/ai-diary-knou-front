63699ebb92de9b13965867111651aaca0c9539ac
silveriron <kichul7493@gmail.com>
2024-07-24 23:38:00 +0900

    fix: 하단 네비게이션 페이지별로 출력 분기

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navigation from "./Navigation";
 
 const Layout = () => {
+  const location = useLocation();
+
+  const isShowNavigation =
+    location.pathname !== "/login" &&
+    location.pathname !== "/regist" &&
+    location.pathname !== "/forgotPassword" &&
+    location.pathname !== "/onboarding";
+
   return (
     <main className="max-w-md mx-auto min-h-screen overflow-auto">
       <Outlet />
-      <Navigation />
+      {isShowNavigation && <Navigation />}
     </main>
   );
 };
