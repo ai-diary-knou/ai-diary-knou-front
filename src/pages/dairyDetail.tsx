d29180c632ab074e9a5fff889b99c2945c93ef6f
silveriron <kichul7493@gmail.com>
2024-08-02 00:19:35 +0900

    fix: console 삭제

--- src/pages/dairyDetail.tsx
+++ src/pages/dairyDetail.tsx
@@ -18,8 +18,6 @@ const DairyDetailPage = () => {
     enabled: dairyId !== undefined,
   });
 
-  console.log(data);
-
   if (isLoading) {
     return (
       <div className="w-full h-full flex justify-center items-center">
